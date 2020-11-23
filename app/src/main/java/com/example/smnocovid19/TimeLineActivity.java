3b86997049d553cfbe175eb8848d436fbdc1b66a
toppms <top1604@naver.com>
2020-11-23 22:53:55 +0900

    timeline update

--- app/src/main/java/com/example/smnocovid19/TimeLineActivity.java
+++ app/src/main/java/com/example/smnocovid19/TimeLineActivity.java
@@ -3,6 +3,7 @@ package com.example.smnocovid19;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
@@ -16,48 +17,12 @@ import com.google.firebase.database.FirebaseDatabase;
 
 import java.sql.Time;
 import java.util.ArrayList;
+import java.util.List;
 
 public class TimeLineActivity extends AppCompatActivity {
-    ArrayList<String> myArrayList = new ArrayList<>();
-    ListView listView;
-    private DatabaseReference mRef;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_time_line);
-        final ArrayAdapter<String> myArrayAdapter = new ArrayAdapter<String>(TimeLineActivity.this, android.R.layout.simple_list_item_1,myArrayList);
-
-        listView = findViewById(R.id.timeLineList);
-        mRef = FirebaseDatabase.getInstance().getReference();
-        mRef.addChildEventListener(new ChildEventListener() {
-
-            @Override
-            public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
-                String value = snapshot.getValue(String.class);
-                myArrayList.add(value);
-                listView.setAdapter(myArrayAdapter);
-                myArrayAdapter.notifyDataSetChanged();
-            }
-
-            @Override
-            public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
-
-            }
-
-            @Override
-            public void onChildRemoved(@NonNull DataSnapshot snapshot) {
-
-            }
-
-            @Override
-            public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
-
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-
-            }
-        });
     }
 }
