6ade0bf882ecdea22e986ed4600ca267c3c83974
dongmin <qweuio456@naver.com>
2020-11-23 22:16:16 +0900

    QR코드 인식 기능 구현

--- app/src/main/java/com/example/smnocovid19/QrCodeActivity.java
+++ app/src/main/java/com/example/smnocovid19/QrCodeActivity.java
@@ -2,13 +2,39 @@ package com.example.smnocovid19;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
 public class QrCodeActivity extends AppCompatActivity {
-
+    private IntentIntegrator qrScan;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_qrcode);
+
+        qrScan = new IntentIntegrator(this);
+        qrScan.setOrientationLocked(false);
+        qrScan.setPrompt("사각형에 QR코드를 맞춰주세요");
+        qrScan.initiateScan();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if (result != null) {
+            if (result.getContents() == null) {
+                Toast.makeText(this, "취소하였습니다", Toast.LENGTH_LONG).show();
+                //todo
+            } else {
+                Toast.makeText(this, "인식되었습니다 " + result.getContents(), Toast.LENGTH_LONG).show();
+                //todo
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
     }
 }
