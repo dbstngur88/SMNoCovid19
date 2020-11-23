d0a5c2a8ba77eacdfd274daf041879b25ba1d082
suhyeokyoon <dbstngur88@naver.com>
2020-11-23 20:13:28 +0900

    Create QrCodeActivity, TimeLineActivity & link to Main

--- app/src/main/java/com/example/smnocovid19/MainActivity.java
+++ app/src/main/java/com/example/smnocovid19/MainActivity.java
@@ -2,14 +2,42 @@ package com.example.smnocovid19;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button button1, button2;
+    Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        button1 = findViewById(R.id.button1);
+        button2 = findViewById(R.id.button2);
+        
+        intent = new Intent();
+        
+        //장동민 개발 내역 연결 버튼
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                intent = new Intent(MainActivity.this, QrCodeActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        //박민석 개발 내역 연결 버튼
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                intent = new Intent(MainActivity.this, TimeLineActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 } //test commit 장동민
  // second test 장동민
---
+++ app/src/main/java/com/example/smnocovid19/TimeLineActivity.java
@@ -0,0 +1,14 @@
+package com.example.smnocovid19;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TimeLineActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_time_line);
+    }
+}
