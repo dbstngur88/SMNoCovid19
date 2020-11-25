28e16a8da8891c972b2a463fad2bae389b06f823
dongmin <qweuio456@naver.com>
2020-11-25 14:21:31 +0900

    add User.java, activity_qrcode.xml, QrCodeActivity - add JSON

--- app/src/main/java/com/example/smnocovid19/QrCodeActivity.java
+++ app/src/main/java/com/example/smnocovid19/QrCodeActivity.java
@@ -4,17 +4,69 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import static java.security.AccessController.getContext;
+
 public class QrCodeActivity extends AppCompatActivity {
+    private Button btn_check;
+    private TextView textViewUserNumber, textViewBuildingFloor, textViewBuildingName, textViewUpdateTime;
     private IntentIntegrator qrScan;
+    private DatabaseReference mDatabase;
+    int a = 0;
+
+    long now = System.currentTimeMillis();
+    Date date = new Date(now);
+    SimpleDateFormat sdfNow = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+    String formatDate = sdfNow.format(date); // 현재시간을 가지고있음.
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_qrcode);
+
+        btn_check = findViewById(R.id.check);
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+        //readUser();
+
+        btn_check.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String getUserNumber = textViewUserNumber.getText().toString();
+                String getBuildingFloor = textViewBuildingFloor.getText().toString();
+                String getBuildingName = textViewBuildingName.getText().toString();
+                String getUpdateTime = textViewUpdateTime.getText().toString();
+
+                HashMap result = new HashMap<>();
+                result.put("학번", getUserNumber);
+                result.put("층", getBuildingFloor);
+                result.put("건물", getBuildingName);
+                result.put("입장시간", getUpdateTime);
+
+                a = a + 1;
+                writeNewUser(a, getUserNumber, getBuildingFloor, getBuildingName, getUpdateTime);
+
+            }
+        });
+
+       // textViewUserNumber = (TextView) findViewById(R.id.textViewUserNumber);
+        textViewBuildingFloor = (TextView) findViewById(R.id.textViewBuildingFloor);
+        textViewBuildingName = (TextView) findViewById(R.id.textViewBuildingName);
+        textViewUpdateTime = (TextView) findViewById(R.id.textViewUpdateTime);
 
         qrScan = new IntentIntegrator(this);
         qrScan.setOrientationLocked(false);
@@ -27,14 +79,31 @@ public class QrCodeActivity extends AppCompatActivity {
         IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
         if (result != null) {
             if (result.getContents() == null) {
-                Toast.makeText(this, "취소하였습니다", Toast.LENGTH_LONG).show();
-                //todo
+                Toast.makeText(this, "취소하였습니다", Toast.LENGTH_SHORT).show();
             } else {
-                Toast.makeText(this, "인식되었습니다 " + result.getContents(), Toast.LENGTH_LONG).show();
-                //todo
+                Toast.makeText(this, "인식되었습니다 " + result.getContents(), Toast.LENGTH_SHORT).show();
+                try {
+                    JSONObject obj = new JSONObject(result.getContents());
+                    //textViewUserNumber.setText(obj.getString(""));
+                    textViewBuildingFloor.setText(obj.getString("buildingFloor"));
+                    textViewBuildingName.setText(obj.getString("buildingName"));
+                    textViewUpdateTime.setText(obj.getString("updateDate"));
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                    // textViewUserNumber.setText(result.getContents());
+                }
             }
         } else {
             super.onActivityResult(requestCode, resultCode, data);
         }
     }
+
+    private void writeNewUser(int userId, String getUserNumber, String getBuildingFloor, String getBuildingName, String getUpdateTime) {
+
+    }
 }
+
+//    private void readUser() {
+//
+//    }
+
---
+++ app/src/main/java/com/example/smnocovid19/User.java
@@ -0,0 +1,62 @@
+package com.example.smnocovid19;
+
+public class User {
+    public String userNumber;
+    public String buildingFloor;
+    public String buildingName;
+    public String updateTime;
+
+    public User() {
+
+    }
+
+    public User(String userNumber, String buildingFloor, String buildingName, String updateTime) {
+        this.userNumber = userNumber;
+        this.buildingFloor = buildingFloor;
+        this.buildingName = buildingName;
+        this.updateTime = updateTime;
+    }
+
+    public String getUserNumber() {
+        return userNumber;
+    }
+
+    public void setUserNumber(String userNumber) {
+        this.userNumber = userNumber;
+    }
+
+    public String getBuildingFloor() {
+        return buildingFloor;
+    }
+
+    public void setBuildingFloor(String buildingFloor) {
+        this.buildingFloor = buildingFloor;
+    }
+
+    public String getBuildingName() {
+        return buildingName;
+    }
+
+    public void setBuildingName(String buildingName) {
+        this.buildingName = buildingName;
+    }
+
+    public String getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(String updateTime) {
+        this.updateTime = updateTime;
+    }
+
+    @Override
+    public String toString() {
+        return "timeline{" +
+                "userNumber ='" + userNumber + '\'' +
+                ", buildingFloor ='" + buildingFloor + '\'' +
+                ", buildingName ='" + buildingName + '\'' +
+                ", updateTime ='" + updateTime + '\'' +
+                '}';
+    }
+
+}
