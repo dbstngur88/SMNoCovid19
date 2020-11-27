14e0286b86eb97f9d24a08f7c81a20be9f4d06d3
suhyeokyoon <dbstngur88@naver.com>
2020-11-28 00:51:07 +0900

    Print Major&Name On MainActivity

--- app/src/main/java/com/example/smnocovid19/MainActivity.java
+++ app/src/main/java/com/example/smnocovid19/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.smnocovid19;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -7,33 +9,55 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 import com.google.firebase.iid.FirebaseInstanceId;
 
 public class MainActivity extends AppCompatActivity {
-    Button button1, button2, button3, button4;
+    Button button1, button2, button3;
     Intent intent;
     private FirebaseAuth fAuth;
+    FirebaseFirestore fStore;
+    String userEmail;
+    String fStoreName;
+    String fStoreMajor;
+
+    TextView viewUserInfo;
     //현재 로그인 된 유저 정보를 담을 변수
     private FirebaseUser currentUser;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
-
         super.onCreate(savedInstanceState);
         String token = FirebaseInstanceId.getInstance().getToken();
         Log.d("토큰정보:", token);
         setContentView(R.layout.activity_main);
-        
+
+        //액션바 안보이게 지정
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+
         button1 = findViewById(R.id.button1);
         button2 = findViewById(R.id.button2);
         button3 = findViewById(R.id.button3);
-        button4 = findViewById(R.id.button4);
-
+        viewUserInfo = findViewById(R.id.userInfo);
         fAuth = FirebaseAuth.getInstance();
+        fStore = FirebaseFirestore.getInstance();
+
+        //userEmail에 유저의 이메일 값 저장
+        currentUser = fAuth.getCurrentUser();
+        userEmail = currentUser.getEmail();
+
+        setUserInfo();
 
         intent = new Intent();
         
@@ -64,10 +88,11 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        //윤수혁 개발 내역 연결 버튼
-        button4.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+    }
+
+    public void mClick(View view) {
+        switch (view.getId()){
+            case R.id.imgSignOut :      //로그아웃
                 fAuth.getInstance().signOut();
                 // Check if user is signed in (non-null) and update UI accordingly.
                 currentUser = fAuth.getCurrentUser();
@@ -78,8 +103,29 @@ public class MainActivity extends AppCompatActivity {
                     startActivity(intent);
                     finish();
                 }
-            }
-        });
+                break;
+        }
+    }
+
+    public void setUserInfo(){
+        fStore.collection("users")     //TestCode, 수정필요(courseList ->competition)
+                .whereEqualTo("email", userEmail)
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                fStoreName = (String) document.get("name");
+                                fStoreMajor = (String) document.get("major");
+
+                                viewUserInfo.setText(fStoreMajor + "\n" + fStoreName + "님, 환영합니다.") ;
+                            }
+                        } else {
+                            Toast.makeText(MainActivity.this,"유저 정보 출력 오류",Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
     }
 } //test commit 장동민
  // second test 장동민
